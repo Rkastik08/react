3df3648e48e0622df8f4717b93bd75f23b321f61
Astik08 <astikgoswami2004@gmail.com>
2024-12-01 01:42:41 +0530

    done

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+import Index from './Index.jsx'
+function App() {
+  
+
+  return (
+  <Index/> //jsx requires only one element to be returned. so we can use <div> but instead of writing div again and again we can write FRAGMENTS <> </> this.
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Index.jsx
@@ -0,0 +1,7 @@
+ function Index(){ //every function should start with Capital letter
+    return (
+        <h3>Astik is ready for react</h3>
+    )
+ }
+
+ export default Index
