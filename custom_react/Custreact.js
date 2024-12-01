a342017962deca852325c246df8c1f9c728516cf
Astik08 <astikgoswami2004@gmail.com>
2024-12-01 17:04:14 +0530

    done

--- custom_react/Custreact.js
+++ custom_react/Custreact.js
@@ -1,6 +1,31 @@
+function customRender(reactElement,Container){
+    /*const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    Container.appendChild(domElement)
+*/
+
+//v-2.0
+
+const domElement = document.createElement(reactElement.type)
+domElement.innerHTML = reactElement.children
+for (const prop in reactElement.props) {
+    domElement.setAttribute(prop , reactElement.props[prop])
+}
+Container.appendChild(domElement)
+}
+
 const reactElement = {
     type: 'a',
-    
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
 }
 
 const mainContainer = document.querySelector('#root')
+
+customRender(reactElement,mainContainer)
